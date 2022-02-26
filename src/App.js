397192993fb217063c32bcf599a047bc58f5b03c
Leon <hualiang.tian@gmail.com>
2022-02-26 18:43:18 -0500

    commit tree 92c35ea18d59cfa09b22124e10ca0b7d40986c6b

--- src/App.js
+++ src/App.js
@@ -1,11 +1,96 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import './App.css';
+// import {addTodo} from "./actions/todos";
+
+// class App extends Component {
+
+//   state = {
+//     todo: ''
+//   }
+
+//   // addTodo = () => {
+//   //   return ({
+//   //     type: 'ADD_TODO',
+//   //     todo: this.state.todo
+//   //   })
+//   // }
+
+//   handleOnChange = event => {
+//     this.setState({
+//       todo: event.target.value
+//     });
+//   }
+
+//   // handleOnSubmit = event => {
+//   //   event.preventDefault();
+//   //   console.log("Todo being added: ", this.state.todo);
+//   //   this.props.dispatch(addTodo(this.state.todo));
+//   //   this.setState({ todo: '' });
+//   // }
+
+//   handleOnSubmit = event => {
+//     event.preventDefault();
+//     console.log("Todo being added: ", this.state.todo);
+//     this.props.addTodo(this.state.todo);
+//     this.setState({ todo: '' });
+//   }
+
+// render() {
+//   debugger;
+//   const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
+//   return (
+//     <div className="App">
+//     <form onSubmit={(event) => this.handleOnSubmit(event)}>
+//       <input
+//         type="text"
+//         onChange={(event) => this.handleOnChange(event)}
+//         id="todos"
+//         placeholder="add todo" 
+//         value={this.state.todo}/>
+//       <input type="submit" />
+//     </form>
+//     <h2>Todos:</h2>
+//       <ol>{renderTodos()}</ol>
+//     </div>
+//   );
+// }
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     todos: state.todos
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: (todo) => {
+//       dispatch(addTodo(todo))
+//     }
+//   };
+// };
+
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todos';
 
 class App extends Component {
 
   state = {
     todo: ''
+  }
+
+  addTodo = () => {
+    return ({
+      type: 'ADD_TODO',
+      todo: this.state.todo
+    })
   }
 
   handleOnChange = event => {
@@ -17,7 +102,7 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.addTodo(this.state.todo);
     this.setState({ todo: '' });
   }
 
@@ -41,10 +126,4 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
